885d8a499b41abe21175957eed93d7bd1b1e6976
Chad Coates <chroco@gmail.com>
2018-04-09 20:16:52 -0700

    encoder integrated

--- src/f0/app_oresat_acs/main.c
+++ src/f0/app_oresat_acs/main.c
@@ -34,16 +34,13 @@
 //=== ChibiOS header files
 #include "ch.h"
 #include "hal.h"
-#include "chprintf.h"
 
 //=== Project header files
 #include "oresat.h"
 #include "acs.h"
-#include "acs_bldc.h"
 
 ACSdata data;
 //uint8_t data[8];
-static uint16_t rxbuf[2];
 
 static SerialConfig ser_cfg = {
 	115200,     //Baud rate
@@ -52,46 +49,9 @@ static SerialConfig ser_cfg = {
 	0,          //
 };
 
-static THD_WORKING_AREA(spi_thread_1_wa, 128);
-static THD_FUNCTION(spi_thread_1, arg) {
-
-  (void)arg;
-  chRegSetThreadName("SPI thread 1");
-
-  uint16_t encoder_val = 0;
-
-  spiStart(&SPID1, &spicfg);                // Start driver.
-  spiAcquireBus(&SPID1);                    // Gain ownership of bus.
-
-  while(true)
-  {
-		rxbuf[0] = 0;
-		spiSelect(&SPID1);                        // Select slave.
-
-		while(SPID1.state != SPI_READY) {}        // Waiting for driver state to be ready.
-		spiReceive(&SPID1,1,rxbuf);               // Receive 1 frame (16 bits).
-		spiUnselect(&SPID1);                      // Unselect slave.
-
-		encoder_val = 0x3FFF & rxbuf[0];
-	 
-		// Display results
-		chprintf(DEBUG_CHP,"Decimal: %u \n", encoder_val);        
-		//chprintf(DEBUG_CHP,"Hex: %x \n", encoder_val);        
-
-		chThdSleepMilliseconds(1000);
-  }
-
-	spiReleaseBus(&SPID1);    // Release ownership of bus.
-	spiStop(&SPID1);          // Stop driver.
-
-}
 static void app_init(void){
-	for(int i = 0; i < 2; ++i){   // Initializing receive buffer to zero. 
-		rxbuf[i] = 0;
-	}
-
-	//	acs_init();	 
-//	bldcInit();
+	acsInit();	 
+	bldcInit();
 	canRPDOObjectInit(CAN_PDO_1,CAN_ID_DEFAULT,8,data.acs);
 //	initTPDO();
 
@@ -99,28 +59,21 @@ static void app_init(void){
 }
 
 static void app_main(void){
-		chThdCreateStatic(
-		spi_thread_1_wa,
-		sizeof(spi_thread_1_wa),
-		NORMALPRIO + 1,
-		spi_thread_1,
-		NULL
-	);
-/*
-	chThdCreateStatic(
-		wa_bldcThread,
-		sizeof(wa_bldcThread), 
-		NORMALPRIO, 
-		bldcThread, 
-		NULL
-	);
-//*/
-/*
+//*
 	chThdCreateStatic(
 		wa_acsThread,
 		sizeof(wa_acsThread), 
 		NORMALPRIO, 
 		acsThread, 
+		NULL
+	);
+//*/
+//*
+	chThdCreateStatic(
+		wa_spiThread,
+		sizeof(wa_spiThread),
+		NORMALPRIO + 1,
+		spiThread,
 		NULL
 	);
 //*/
