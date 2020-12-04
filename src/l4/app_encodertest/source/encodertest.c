df2ec5c5454fce82e0b0d14995f7189f3e7cd541
Armaan <armaan@gmail.com>
2020-12-03 21:22:36 -0800

    beginning to move code over from old ACS-1.0

--- src/l4/app_encodertest/source/encodertest.c
+++ src/l4/app_encodertest/source/encodertest.c
@@ -16,3 +16,36 @@ THD_FUNCTION(blink, arg)
     palClearLine(LINE_LED);
     chThdExit(MSG_OK);
 }
+
+
+
+/**
+ * @brief Handles the SPI transaction, getting the position from the encoder
+ *
+ */
+THD_WORKING_AREA(wa_spiThread,THREAD_SIZE);
+THD_FUNCTION(spiThread,arg){
+  (void)arg;
+	chRegSetThreadName("spiThread");
+
+  spiStart(&SPID1,&spicfg);            	// Start driver.
+  spiAcquireBus(&SPID1);                // Gain ownership of bus.
+
+  while (!chThdShouldTerminateX()) {
+	motor->spi_rxbuf[0] = 0;
+	spiSelect(&SPID1);              // Select slave.
+
+	while(SPID1.state != SPI_READY) {}   
+	spiReceive(&SPID1,1,motor->spi_rxbuf);  // Receive 1 frame (16 bits).
+	spiUnselect(&SPID1);                	// Unselect slave.
+
+	/// TODO: figure out what this does. assume that 0x3FFF is an offset, 
+	/// possibly to account for algular misalightment between the encoder 
+	/// and motor top dead center?
+	motor->position = 0x3FFF & motor->spi_rxbuf[0];
+	
+  }
+
+	spiReleaseBus(&SPID1);    // Release ownership of bus.
+	spiStop(&SPID1);          // Stop driver.
+}
--- src/l4/app_encodertest/source/encodertest.h
+++ src/l4/app_encodertest/source/encodertest.h
@@ -4,8 +4,17 @@
 #include "ch.h"
 #include "hal.h"
 
+/// TODO: had to reduce this to compile with new ADC code
+/// We should figure out what an actual good value is
+#define THREAD_SIZE	(96)
+
 /* Example blinker thread prototypes */
-extern THD_WORKING_AREA(blink_wa, 0x40);
-extern THD_FUNCTION(blink, arg);
+//extern THD_WORKING_AREA(blink_wa, 0x40);
+//extern THD_FUNCTION(blink, arg);
+
+
+extern THD_WORKING_AREA(wa_spiThread,THREAD_SIZE);
+/// Prototype for spi thread function.
+extern THD_FUNCTION(spiThread,arg);
 
 #endif
